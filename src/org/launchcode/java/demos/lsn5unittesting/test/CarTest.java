89c1a9a9c30f537b99814c2587802cad197370b2
vaishnavi <vaishu.srini15@gmail.com>
2021-08-12 06:42:04 -0500

    modified classes in lsn5unittesting

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -1,11 +1,53 @@
 package org.launchcode.java.demos.lsn5unittesting.test;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.*;
+
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
+
 public class CarTest {
+    Car test_car;
+
+    @Before
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    @Test
+    public void emptyTest(){
+        assertEquals(10,10,0.001);
+    }
     //TODO: constructor sets gasTankLevel properly
+
+    @Test
+    public void testInitialGasTank() {
+
+        assertEquals(10, test_car.getGasTankLevel(), .001);
+//        assertTrue("Condition Fails when not equal to 10", test_car.getGasTankLevel()==10);
+//        assertFalse("Condition fails when it is equal to 0",test_car.getGasTankLevel()==0);
+    }
+
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testTankAfterDriving(){
+        test_car.drive(50);
+        assertEquals(9,test_car.getGasTankLevel(),0.001);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void testGasTankAfterExceedingTankRange(){
+        test_car.drive(501);
+        assertEquals(0,test_car.getGasTankLevel(),0.001);
+    }
     //TODO: can't have more gas than tank size, expect an exception
+    @Test(expected = IllegalArgumentException.class)
+    public void testGasOverfillException() {
+        test_car.addGas(5);
+        fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    }
 
 }
